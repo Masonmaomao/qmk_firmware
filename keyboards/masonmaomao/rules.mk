f7fd38743d03187fd3da308e9805e245763305ff
Masonmaomao <mh_mason@163.com>
2023-01-10 16:24:52 +0800

    Add initial rules.mk

--- keyboards/masonmaomao/rules.mk
+++ keyboards/masonmaomao/rules.mk
@@ -1,6 +1,7 @@
 # MCU name
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 MCU = STM32F072
 
 # Bootloader selection
@@ -8,19 +9,25 @@ BOOTLOADER = stm32-dfu
 =======
 =======
 >>>>>>> 62c7ae0a69 (Add initial rules.mk)
+=======
+>>>>>>> 92a127c1fb (Add initial rules.mk)
 MCU = atmega32u4
 
 # Bootloader selection
 BOOTLOADER = atmel-dfu
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 909bb8056e (Add initial rules.mk)
 =======
 >>>>>>> 62c7ae0a69 (Add initial rules.mk)
+=======
+>>>>>>> 92a127c1fb (Add initial rules.mk)
 
 # Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 MOUSEKEY_ENABLE = yes       # Mouse keys
@@ -39,6 +46,8 @@ OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 =======
 =======
 >>>>>>> 62c7ae0a69 (Add initial rules.mk)
+=======
+>>>>>>> 92a127c1fb (Add initial rules.mk)
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -48,6 +57,9 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 909bb8056e (Add initial rules.mk)
 =======
 >>>>>>> 62c7ae0a69 (Add initial rules.mk)
+=======
+>>>>>>> 92a127c1fb (Add initial rules.mk)

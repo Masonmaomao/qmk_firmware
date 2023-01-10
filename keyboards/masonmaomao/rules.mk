1c4d0c388bd4af6a50a8e00a0bd4de57ded97378
Masonmaomao <mh_mason@163.com>
2023-01-10 16:24:52 +0800

    Add initial rules.mk

--- keyboards/masonmaomao/rules.mk
+++ keyboards/masonmaomao/rules.mk
@@ -1,13 +1,21 @@
 # MCU name
+<<<<<<< HEAD
 MCU = STM32F072
 
 # Bootloader selection
 BOOTLOADER = stm32-dfu
+=======
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+>>>>>>> 909bb8056e (Add initial rules.mk)
 
 # Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+<<<<<<< HEAD
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -21,3 +29,13 @@ AUDIO_ENABLE = no           # Audio output
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+=======
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+>>>>>>> 909bb8056e (Add initial rules.mk)

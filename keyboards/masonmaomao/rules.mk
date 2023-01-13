09241799cf08d3bb7b6ac8f9b022027f1891ef58
Masonmaomao <mh_mason@163.com>
2023-01-13 11:12:21 +0800

    Modify the rule.mk: 1, MCU to STM32F072, 2, Enable RGBMATRIX.

--- keyboards/masonmaomao/rules.mk
+++ keyboards/masonmaomao/rules.mk
@@ -1,18 +1,23 @@
 # MCU name
-MCU = atmega32u4
+MCU = STM32F072
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
+BOOTLOADER = stm32-dfu
 
 # Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGBMATIX_ENABLE = yes       # Enable keyboard RGB Matrix
+RGB_MATRIX_DRIVER = pwm     # Define the RGB Driver mode as pwm
 AUDIO_ENABLE = no           # Audio output
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
